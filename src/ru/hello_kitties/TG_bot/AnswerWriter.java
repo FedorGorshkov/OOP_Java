c2eac9e9a23d99b549babd2a0b1e631852e8d1b4
FedorGorshkov <75787257+FedorGorshkov@users.noreply.github.com>
2023-10-16 02:34:42 +0500

    Delete src/ru/hello_kitties/TG_bot directory

--- src/ru/hello_kitties/TG_bot/AnswerWriter.java
+++
@@ -1,6 +0,0 @@
-package ru.hello_kitties.TG_bot;
-import ru.hello_kitties.TG_bot.logic.BotResponse;
-
-public interface AnswerWriter {
-    void writeAnswer(BotResponse response);
-}
--- src/ru/hello_kitties/TG_bot/MessageHandler.java
+++
@@ -1,6 +0,0 @@
-package ru.hello_kitties.TG_bot;
-import ru.hello_kitties.TG_bot.logic.*;
-
-public interface MessageHandler {
-    void handle(BotRequest request, AnswerWriter writer);
-}
