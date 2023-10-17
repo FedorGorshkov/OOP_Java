1e357237b918a34c4a97e6dce4d49c31dc9d24b8
kisssass <114352552+kisssass@users.noreply.github.com>
2023-10-17 14:57:22 +0500

    Update BotRequest.java

--- src/main/java/ru/hello_kitties/TG_bot/logic/BotRequest.java
+++ src/main/java/ru/hello_kitties/TG_bot/logic/BotRequest.java
@@ -1,21 +1,19 @@
 package ru.hello_kitties.TG_bot.logic;
 
-import org.telegram.telegrambots.meta.api.objects.Update;
-
 public class BotRequest {
-    private String message;
+    private final String message;
 
-    private String chatId;
+    private final String chatId;
+
+    public BotRequest(String message, String chatId) {
+        this.message = message;
+        this.chatId = chatId;
+    }
 
     public String getId() {
         return chatId;
     }
-    public void setRequest(String req) {
-        this.message = req;
-    }
-    public void setId(String chatId) {
-        this.chatId = chatId;
-    }
+
 
     public String getRequest() {
         return message;
