d1db74fe78fc1a53659463bceca9e750af04482c
kisssass <114352552+kisssass@users.noreply.github.com>
2023-10-17 14:56:00 +0500

    Update Bot.java

--- src/main/java/ru/hello_kitties/TG_bot/telegram/Bot.java
+++ src/main/java/ru/hello_kitties/TG_bot/telegram/Bot.java
@@ -1,43 +1,44 @@
 package ru.hello_kitties.TG_bot.telegram;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.hello_kitties.TG_bot.AnswerWriter;
-import ru.hello_kitties.TG_bot.InputReader;
 import ru.hello_kitties.TG_bot.logic.BotRequest;
 import ru.hello_kitties.TG_bot.logic.BotResponse;
 import ru.hello_kitties.TG_bot.logic.Handler;
 
-public class Bot extends TelegramLongPollingBot {
+public class Bot extends TelegramLongPollingBot implements AnswerWriter {
 
-    final private String BOT_TOKEN = "6455669284:AAHy_S38_L2eLksPvdUnjkpZQrlPS1hzcJ8";
-    final private String BOT_NAME = "hello_kitties_bot";
-
+    final private String BOT_NAME;
+    public Bot(String botName, String botToken){
+        super(botToken);
+        this.BOT_NAME = botName;
+    }
     @Override
     public String getBotUsername() {
         return BOT_NAME;
     }
-    @Override
-    public String getBotToken() {
-        return BOT_TOKEN;
-    }
-
 
     @Override
     public void onUpdateReceived(Update update) {
         try{
-            if(update.hasMessage() && update.getMessage().hasText())
-            {
-                BotRequest request = new BotRequest();
-                InputReader reader = new InputReaderImplements();
-                AnswerWriter answer = new AnswerWriterImplements();
-                Handler handle= new Handler();
-
-                reader.getUserInput(request);
-                handle.handle(request,answer);
+            if(update.hasMessage() && update.getMessage().hasText()) {
+                BotRequest request = new BotRequest(update.getMessage().getText(), "");
+                Handler handle = new Handler();
+                //reader.getUserInput(request);
+                handle.handle(request, this);
             }
         } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void writeAnswer(BotResponse response) {
+        try {
+            execute(new SendMessage("", response.getResponse()));
+        } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
     }
