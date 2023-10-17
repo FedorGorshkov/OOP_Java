eb32f890a62a4cf816f675c58c3258726a6435b5
kisssass <114352552+kisssass@users.noreply.github.com>
2023-10-17 14:58:23 +0500

    Update Main.java

--- src/main/java/ru/hello_kitties/TG_bot/Main.java
+++ src/main/java/ru/hello_kitties/TG_bot/Main.java
@@ -6,14 +6,27 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.hello_kitties.TG_bot.telegram.*;
 import ru.hello_kitties.TG_bot.logic.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String args[])
     {
         try {
+            Properties properties = new Properties();
+            properties.load(new FileInputStream("bot.properties"));
+
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new Bot());
+            telegramBotsApi.registerBot(new Bot("hello_kitties_bot", "6455669284:AAHy_S38_L2eLksPvdUnjkpZQrlPS1hzcJ8"));
         } catch (TelegramApiException e) {
             e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
