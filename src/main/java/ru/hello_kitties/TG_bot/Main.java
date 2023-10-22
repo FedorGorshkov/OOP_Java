f1578c0afc5ad0c633e7c7a36651f1261c8a0150
kisssass <katrula@bk.ru>
2023-10-22 17:42:57 +0500

    try

--- src/main/java/ru/hello_kitties/TG_bot/Main.java
+++ src/main/java/ru/hello_kitties/TG_bot/Main.java
@@ -16,8 +16,8 @@ public class Main {
     public static void main(String args[])
     {
         try {
-            Properties properties = new Properties();
-            properties.load(new FileInputStream("bot.properties"));
+            Properties properties1 = new Properties();
+            properties1.load(new FileInputStream("bot.properties"));
 
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new Bot("hello_kitties_bot", "6455669284:AAHy_S38_L2eLksPvdUnjkpZQrlPS1hzcJ8"));
