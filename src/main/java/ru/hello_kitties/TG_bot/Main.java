97c78af36f38f443034c0fb22d39966582499978
kisssass <katrula@bk.ru>
2023-10-22 23:35:36 +0500

    this shit works

--- src/main/java/ru/hello_kitties/TG_bot/Main.java
+++ src/main/java/ru/hello_kitties/TG_bot/Main.java
@@ -1,16 +1,18 @@
 package ru.hello_kitties.TG_bot;
 
+
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import ru.hello_kitties.TG_bot.telegram.*;
+import org.slf4j.LoggerFactory;
 import ru.hello_kitties.TG_bot.logic.*;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
+
 
 public class Main {
     public static void main(String[] args)
